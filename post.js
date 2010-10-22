d086e8277d098b27203f8c135714d2ba92e0aedf
Tim-Smart <tim@fostle.com>
2010-10-23 00:15:51 +1300

    Post response -> request

--- post.js
+++ post.js
@@ -4,13 +4,13 @@ module.exports = function () {
   return function (request, response, next) {
     var data = '';
 
-    response.setEncoding('utf8');
+    request.setEncoding('utf8');
 
-    response.addListener('data', function (chunk) {
+    request.addListener('data', function (chunk) {
       data += chunk;
     });
 
-    response.addListener('end', function () {
+    request.addListener('end', function () {
       response.body = data;
       next();
     });
